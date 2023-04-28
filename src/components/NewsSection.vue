9891bee41951b23572e81bc55d8a45bba060d58d
nicolaazonta <nicolazonta114@gmail.com>
2023-04-28 10:31:51 +0200

    make utilities dedicated scss file

--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -6,7 +6,31 @@
 
 <template>
     <div id="NewsSection">
-        <h2>NewsSection</h2>
+        <div class="container w-75 py-5">
+            <div class="header_top">
+                <h6 class="text-uppercase py-1">our business area</h6> 
+                <h2 class="text-capitalize py-1">excellence in services</h2>            
+            </div>
+            <div class="header_bottom d-flex justify-content-between align-items-center py-1">
+                <p class="w-75">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo quisquam doloribus quo repellat facilis esse consequatur odit in recusandae voluptate.</p>
+                <button class="full_btn my_btn">see all</button>
+            </div>
+
+            <div class="row py-3 g-4">
+                <div class="col-4">
+                    <div class="card p-4 d-flex flex-column align-items-start">
+                        <h1 class="icon">icon</h1>
+                        <h5>Audit & Assurance</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iusto!</p>
+                        <button class="empty_btn my_btn">read more</button>
+                    </div>
+                </div>
+                
+              
+              
+            </div>
+
+        </div>
     </div>
 </template>
 
