18fd0c346d182ed5afc67f6be22cb50e5daf326b
nicolaazonta <nicolazonta114@gmail.com>
2023-04-28 14:46:07 +0200

    make get in touch section

--- src/components/KnowSection.vue
+++ src/components/KnowSection.vue
@@ -13,9 +13,10 @@
                     <h2 class="section_title">Know <span class="marked">First</span></h2>
                     <p class="section_paragraph">Follow closely and receive content about our company and the news of the current market</p>
                 </div>
-                <div class="col-6 d-flex flex-column h-100 justify-content-between">
-                    <input type="text">
-                    <input type="text">
+                <div class="col-6 d-flex flex-column h-100 justify-content-between align-items-start">
+                    <input type="text" class="border-0 bg-opacity-100 form-control" placeholder="Name" aria-label="name">
+                    <input type="text" class="border-0 bg-opacity-100 form-control" placeholder="Email" aria-label="email">
+
                     <button class="my_btn full_btn">subscribe</button>
                 </div>
             </div>
--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,16 +1,62 @@
 <script>
-    export default {
-        name: 'PageFooter'
-    }
+export default {
+    name: 'PageFooter'
+}
 </script>
 
 <template>
     <div id="PageFooter">
-        <h1>footer</h1>
+        <div class="container w-75 py-5">
+            <div class="row">
+                <div class="col-8 px-5">
+                    <h6 class="section_subtitle">send a message</h6>
+                    <h2 class="section_title">Get in <span class="marked">Touch</span></h2>
+                    <p class="w-75 section_paragraph">We will respond to your message as soon as possible</p>
+                    <div class="form-group">
+                        <div class="row g-2">
+                            <div class="col-6">
+                                <input type="text" class="form-control" placeholder="Name" aria-label="name">
+                            </div>
+                            <div class="col-6">
+                                <input type="text" class="form-control" placeholder="Name" aria-label="name">
+                            </div>
+                            <div class="col-6">
+                                <input type="text" class="form-control" placeholder="Name" aria-label="name">
+                            </div>
+                            <div class="col-6">
+                                <input type="text" class="form-control" placeholder="Name" aria-label="name">
+                            </div>
+                        </div>
+                        <div class="row my-3">
+                            <div class="col">
+                                <textarea class="form-control" placeholder="Name" aria-label="name"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="my_btn full_btn">send</button>
+                </div>
+                <div class="col-4 references">
+                    <h5 class="py-1">Example Inc.</h5>
+                    <p class="py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas ipsam</p>
+                    <p class="py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas ipsam</p>
+                    <div class="contact d-flex align-items-center">
+                        <h2 class="marked rounded-circle">phone</h2>
+                        <span>+1 (305) 1234-5678</span>
+                    </div>
+                    <div class="contact d-flex align-items-center">
+                        <h2 class="marked rounded-circle">mail</h2>
+                        <span>hello@example.com</span>
+                    </div>
+                    <div class="contact d-flex align-items-center">
+                        <h2 class="marked rounded-circle">location</h2>
+                        <span>Main Avenue, 987</span>
+                    </div>
+                    <button class="my_btn empty_btn">view map</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
