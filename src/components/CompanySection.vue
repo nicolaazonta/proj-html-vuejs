774b98ef1c3ca3c8b0ae4a356340f7e7de06ad17
nicolaazonta <nicolazonta114@gmail.com>
2023-04-28 08:59:54 +0200

    position img in company section

--- src/components/CompanySection.vue
+++ src/components/CompanySection.vue
@@ -5,8 +5,8 @@
 </script>
 
 <template>
-    <div id="CompanySection">
-        <div class="container w-75">
+    <div id="CompanySection" class="position-relative">
+        <div class="container w-75 ">
             <div class="left_side"></div>
             <div class="right_side">
                 <img src="../assets/img/about-3.jpg" alt="">
