c672f5695754b39a37f2a468c1d7de09e4cd1d9c
nicolaazonta <nicolazonta114@gmail.com>
2023-04-28 13:31:34 +0200

    make newsletter section

--- src/components/KnowSection.vue
+++ src/components/KnowSection.vue
@@ -6,7 +6,20 @@
 
 <template>
     <div id="KnowSection">
-        <h2>KnowSection</h2>
+        <div class="container w-50">            
+            <div class="row py-5">
+                <div class="col-6">
+                    <h5 class="section_subtitle">newsletter</h5>
+                    <h2 class="section_title">Know <span class="marked">First</span></h2>
+                    <p class="section_paragraph">Follow closely and receive content about our company and the news of the current market</p>
+                </div>
+                <div class="col-6 d-flex flex-column h-100 justify-content-between">
+                    <input type="text">
+                    <input type="text">
+                    <button class="my_btn full_btn">subscribe</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
