2bc222ee8a2723e7b2854cb4d1036f60f234831d
nicolaazonta <nicolazonta114@gmail.com>
2023-04-28 11:35:12 +0200

    make news section

--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -8,21 +8,31 @@
     <div id="NewsSection">
         <div class="container w-75 py-5">
             <div class="header_top">
-                <h6 class="text-uppercase py-1">our business area</h6> 
-                <h2 class="text-capitalize py-1">excellence in services</h2>            
+                <h6 class="section_subtitle">our editorial content</h6> 
+                <h2 class="section_title">Latest News</h2>          
             </div>
             <div class="header_bottom d-flex justify-content-between align-items-center py-1">
-                <p class="w-75">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo quisquam doloribus quo repellat facilis esse consequatur odit in recusandae voluptate.</p>
+                <p class="w-75 section_paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo quisquam doloribus quo .</p>
                 <button class="full_btn my_btn">see all</button>
             </div>
 
             <div class="row py-3 g-4">
                 <div class="col-4">
-                    <div class="card p-4 d-flex flex-column align-items-start">
-                        <h1 class="icon">icon</h1>
-                        <h5>Audit & Assurance</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iusto!</p>
-                        <button class="empty_btn my_btn">read more</button>
+                    <div class="card position-relative">
+                        <img class="card-img" src="../assets/img/news-1.jpg" alt="">
+                        <p class="card-img-overlay text_card_img">Increasing creativity is possible for everyone</p>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card position-relative">
+                        <img class="card-img" src="../assets/img/news-2.jpg" alt="">
+                        <p class="card-img-overlay text_card_img">Because market research is part of the business plan</p>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card position-relative">
+                        <img class="card-img" src="../assets/img/news-3.jpg" alt="">
+                        <p class="card-img-overlay text_card_img">Working from home is now a trend</p>
                     </div>
                 </div>
                 
