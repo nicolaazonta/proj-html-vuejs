c707b01362f47575d592e580f6b6b5ef2d1d544a
nicolaazonta <nicolazonta114@gmail.com>
2023-04-28 10:12:52 +0200

    make pricing section

--- src/components/PricingSection.vue
+++ src/components/PricingSection.vue
@@ -6,7 +6,46 @@
 
 <template>
     <div id="PricingSection">
-        <h2>PricingSection</h2>
+        <div class="container w-75 p-5">
+            <h6 class="text-uppercase text-center">monthly plans</h6>
+            <h2 class="text-capitalize text-center py-2"><span class="marked">pricing</span> table</h2>
+            <p class="text-center px-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente et facilis amet magnam odit dignissimos voluptatibus voluptates vel. Corporis?</p>
+            <div class="row py-3 align-items-center">
+                <div class="col-4">
+                    <div class="card d-flex flex-column align-items-center p-3">
+                        <h1 class="icon">icon</h1>
+                        <h5 class="text-capitalize">standard</h5>
+                        <span class="price">$<span class="big_price">490</span>/mo</span>
+                        <div class="spec d-flex justify-content-between">
+                            <span class="text-capitalize spec_text">standard accounting</span>
+                            <h5 class="icon spec_icon">icon</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card d-flex flex-column align-items-center px-3 py-4">
+                        <h1 class="icon">icon</h1>
+                        <h5 class="text-capitalize">standard</h5>
+                        <span class="price">$<span class="big_price">490</span>/mo</span>
+                        <div class="spec d-flex justify-content-between">
+                            <span class="text-capitalize spec_text">standard accounting</span>
+                            <h5 class="icon spec_icon">icon</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card d-flex flex-column align-items-center p-3">
+                        <h1 class="icon">icon</h1>
+                        <h5 class="text-capitalize">standard</h5>
+                        <span class="price">$<span class="big_price">490</span>/mo</span>
+                        <div class="spec d-flex justify-content-between">
+                            <span class="text-capitalize spec_text">standard accounting</span>
+                            <h5 class="icon spec_icon">icon</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
